ae9cd7c12356d49282be80a6bdc0a46ddf1e9280
Fabiano <contatofasantiago@hotmail.com>
2024-09-17 11:04:18 -0300

    Remove superfluous comments from code to enhance readability

--- NOC_Email/MainForm.cs
+++ NOC_Email/MainForm.cs
@@ -3,8 +3,6 @@
  * User: fjstavares
  * Date: 17/09/2024
  * Time: 10:57
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
 using System.Collections.Generic;
@@ -13,21 +11,11 @@ using System.Windows.Forms;
 
 namespace NOC_Email
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
 	public partial class MainForm : Form
 	{
 		public MainForm()
 		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
 			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
 		}
 	}
 }
