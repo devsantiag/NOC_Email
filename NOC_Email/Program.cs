8fc02a5ed7438ce50a4b0bf4d23c72176f534eac
Fabiano <contatofasantiago@hotmail.com>
2024-09-19 14:33:40 -0300

    Taken from Comments Instructions

--- NOC_Email/Program.cs
+++ NOC_Email/Program.cs
@@ -3,22 +3,14 @@
  * User: fjstavares
  * Date: 17/09/2024
  * Time: 10:57
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
 using System.Windows.Forms;
 
 namespace NOC_Email
 {
-	/// <summary>
-	/// Class with program entry point.
-	/// </summary>
 	internal sealed class Program
 	{
-		/// <summary>
-		/// Program entry point.
-		/// </summary>
 		[STAThread]
 		private static void Main(string[] args)
 		{
