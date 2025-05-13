6eb3d0816e61a39dc45da97750623423d58055a8
santiago <98044979+devsantiag@users.noreply.github.com>
2025-05-13 11:42:25 -0300

    refactor(ui): restrict form dragging to panelConfiguracoes and clean up comments

--- NOC_Email/NotificarClientePorMensagemDeTexto.Designer.cs
+++ NOC_Email/NotificarClientePorMensagemDeTexto.Designer.cs
@@ -20,6 +20,9 @@ namespace NOC_Email
 		private System.Windows.Forms.TextBox textBox1_TitutloEmail;
 		private System.Windows.Forms.Button btnAnexar;
 		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Panel panelConfiguracoes;
+		private System.Windows.Forms.Label btnCloseWindow;
+		private System.Windows.Forms.Label label11;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -48,12 +51,16 @@ namespace NOC_Email
 			this.textBox1_TitutloEmail = new System.Windows.Forms.TextBox();
 			this.btnAnexar = new System.Windows.Forms.Button();
 			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.panelConfiguracoes = new System.Windows.Forms.Panel();
+			this.btnCloseWindow = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.panelConfiguracoes.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// richTextBox1_CorpoDeMensagemDeTexto
 			// 
 			this.richTextBox1_CorpoDeMensagemDeTexto.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.richTextBox1_CorpoDeMensagemDeTexto.Location = new System.Drawing.Point(12, 45);
+			this.richTextBox1_CorpoDeMensagemDeTexto.Location = new System.Drawing.Point(4, 68);
 			this.richTextBox1_CorpoDeMensagemDeTexto.Name = "richTextBox1_CorpoDeMensagemDeTexto";
 			this.richTextBox1_CorpoDeMensagemDeTexto.Size = new System.Drawing.Size(482, 164);
 			this.richTextBox1_CorpoDeMensagemDeTexto.TabIndex = 0;
@@ -62,7 +69,7 @@ namespace NOC_Email
 			// btnApagarMensagem
 			// 
 			this.btnApagarMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnApagarMensagem.Location = new System.Drawing.Point(360, 215);
+			this.btnApagarMensagem.Location = new System.Drawing.Point(352, 238);
 			this.btnApagarMensagem.Name = "btnApagarMensagem";
 			this.btnApagarMensagem.Size = new System.Drawing.Size(134, 47);
 			this.btnApagarMensagem.TabIndex = 1;
@@ -73,7 +80,7 @@ namespace NOC_Email
 			// btnEncaminharEmail
 			// 
 			this.btnEncaminharEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnEncaminharEmail.Location = new System.Drawing.Point(220, 215);
+			this.btnEncaminharEmail.Location = new System.Drawing.Point(212, 238);
 			this.btnEncaminharEmail.Name = "btnEncaminharEmail";
 			this.btnEncaminharEmail.Size = new System.Drawing.Size(134, 47);
 			this.btnEncaminharEmail.TabIndex = 2;
@@ -84,7 +91,7 @@ namespace NOC_Email
 			// textBox1_TitutloEmail
 			// 
 			this.textBox1_TitutloEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.textBox1_TitutloEmail.Location = new System.Drawing.Point(12, 12);
+			this.textBox1_TitutloEmail.Location = new System.Drawing.Point(4, 35);
 			this.textBox1_TitutloEmail.Name = "textBox1_TitutloEmail";
 			this.textBox1_TitutloEmail.Size = new System.Drawing.Size(482, 26);
 			this.textBox1_TitutloEmail.TabIndex = 3;
@@ -92,7 +99,7 @@ namespace NOC_Email
 			// btnAnexar
 			// 
 			this.btnAnexar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnAnexar.Location = new System.Drawing.Point(80, 215);
+			this.btnAnexar.Location = new System.Drawing.Point(72, 238);
 			this.btnAnexar.Name = "btnAnexar";
 			this.btnAnexar.Size = new System.Drawing.Size(134, 47);
 			this.btnAnexar.TabIndex = 4;
@@ -104,22 +111,59 @@ namespace NOC_Email
 			// 
 			this.openFileDialog1.FileName = "openFileDialog_AnexarArquivo";
 			// 
+			// panelConfiguracoes
+			// 
+			this.panelConfiguracoes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(62)))), ((int)(((byte)(93)))));
+			this.panelConfiguracoes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.panelConfiguracoes.Controls.Add(this.btnCloseWindow);
+			this.panelConfiguracoes.Controls.Add(this.label11);
+			this.panelConfiguracoes.Location = new System.Drawing.Point(-1, 0);
+			this.panelConfiguracoes.Name = "panelConfiguracoes";
+			this.panelConfiguracoes.Size = new System.Drawing.Size(492, 32);
+			this.panelConfiguracoes.TabIndex = 24;
+			// 
+			// btnCloseWindow
+			// 
+			this.btnCloseWindow.AutoSize = true;
+			this.btnCloseWindow.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnCloseWindow.Location = new System.Drawing.Point(459, 1);
+			this.btnCloseWindow.Name = "btnCloseWindow";
+			this.btnCloseWindow.Size = new System.Drawing.Size(25, 30);
+			this.btnCloseWindow.TabIndex = 23;
+			this.btnCloseWindow.Text = "X";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label11.ForeColor = System.Drawing.Color.White;
+			this.label11.Location = new System.Drawing.Point(3, 3);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(375, 25);
+			this.label11.TabIndex = 23;
+			this.label11.Text = "NOC | Comunicação com Cliente via E-mail";
+			// 
 			// NotificarClientePorMensagemDeTexto
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.AutoSize = true;
-			this.ClientSize = new System.Drawing.Size(506, 267);
+			this.BackColor = System.Drawing.SystemColors.ControlLight;
+			this.ClientSize = new System.Drawing.Size(490, 296);
 			this.Controls.Add(this.btnAnexar);
 			this.Controls.Add(this.textBox1_TitutloEmail);
 			this.Controls.Add(this.btnEncaminharEmail);
 			this.Controls.Add(this.btnApagarMensagem);
 			this.Controls.Add(this.richTextBox1_CorpoDeMensagemDeTexto);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Controls.Add(this.panelConfiguracoes);
+			this.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "NotificarClientePorMensagemDeTexto";
 			this.Text = "NOC | Comunicação com Cliente via E-mail";
+			this.panelConfiguracoes.ResumeLayout(false);
+			this.panelConfiguracoes.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
--- NOC_Email/NotificarClientePorMensagemDeTexto.cs
+++ NOC_Email/NotificarClientePorMensagemDeTexto.cs
@@ -3,6 +3,8 @@ using System;
 using System.Windows.Forms;
 using Outlook = Microsoft.Office.Interop.Outlook; // Alias para facilitar a chamada do Outlook
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
 
 namespace NOC_Email
 {
@@ -10,14 +12,33 @@ namespace NOC_Email
 	{
 		// Lista que armazena os caminhos dos arquivos anexados
 		private List<string> arquivoAnexadoPeloAgente = new List<string>();
+		
+		[DllImport("user32.dll")]
+		public static extern bool ReleaseCapture();
+
+		[DllImport("user32.dll")]
+		public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+		private const int WM_NCLBUTTONDOWN = 0xA1;
+		private const int HTCAPTION = 0x2;
 
 		// Construtor do formulário
 		public NotificarClientePorMensagemDeTexto()
 		{
-			InitializeComponent(); // Inicializa os componentes do formulário
-			OrdenarTabIndex(); // Define a ordem de navegação via tecla TAB
+			InitializeComponent(); 
+			OrdenarTabIndex(); 
+			panelConfiguracoes.MouseDown += PanelConfiguracoes_MouseDown;
 		}
 
+		private void PanelConfiguracoes_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				ReleaseCapture();
+				SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+			}
+		}
+		
 		// Botão que dispara o envio do e-mail
 		private void BtnEncaminharEmailClick(object sender, EventArgs e)
 		{
@@ -29,37 +50,28 @@ namespace NOC_Email
 			}
 
 			try {
-				// Captura os valores dos campos
 				string tituloEmail = textBox1_TitutloEmail.Text;
 				string corpoMensagemTexto = richTextBox1_CorpoDeMensagemDeTexto.Text;
 
-				// Codifica os dados para compatibilidade com URI, caso fosse usado um link mailto (neste caso, está redundante)
 				string encodedTitulo = Uri.EscapeDataString(tituloEmail);
 				string encodedCorpoMensagem = Uri.EscapeDataString(corpoMensagemTexto);
-
-				// Cria uma instância do Outlook e um novo e-mail
 				Outlook.Application outLookApp = new Outlook.Application();
 				Outlook.MailItem email = (Outlook.MailItem)outLookApp.CreateItem(Outlook.OlItemType.olMailItem);
 
-				// Define o título e corpo da mensagem
 				email.Subject = tituloEmail.ToUpper();
 				email.Body = corpoMensagemTexto;
 
-				// Adiciona os anexos à mensagem
 				foreach (string arquivo in arquivoAnexadoPeloAgente)
 				{
-				    email.Attachments.Add(arquivo);
+					email.Attachments.Add(arquivo);
 				}
 
-				// Exibe a janela do Outlook com a mensagem pronta (não envia automaticamente)
 				email.Display(false);
 
-				// (Opcional) Criação de link "mailto" - atualmente não está sendo usado
 				string mailtoLink = string.Format("outlook:?subject={0}&body={1}", encodedTitulo, encodedCorpoMensagem);
-			} 
+			}
 			catch (Exception ex)
 			{
-				// Exibe mensagem de erro ao usuário
 				MessageBox.Show("Erro ao tentar encaminhar o Email: " + ex.Message);
 				throw;
 			}
@@ -68,9 +80,6 @@ namespace NOC_Email
 		// Botão que limpa os campos do formulário e os anexos
 		void BtnApagarMensagemClick(object sender, EventArgs e)
 		{
-			ApagarConteudo(); // Limpa título e corpo do texto
-			arquivoAnexadoPeloAgente.Clear(); // Limpa a lista de anexos
-			btnAnexar.Text = "Anexar"; // Reseta o texto do botão de anexo
 		}
 
 		// Função que limpa os campos do título e corpo da mensagem
@@ -94,15 +103,15 @@ namespace NOC_Email
 		{
 			OpenFileDialog dialogo = new OpenFileDialog();
 			dialogo.Title = "Selecione o(s) arquivo(s)";
-			dialogo.Filter = "Todos os arquivos (*.*)|*.*"; // Permite todos os tipos de arquivos
-			dialogo.Multiselect = true; // Permite seleção múltipla
+			dialogo.Filter = "Todos os arquivos (*.*)|*.*"; 
+			dialogo.Multiselect = true; 
 
 			if (dialogo.ShowDialog() == DialogResult.OK)
 			{
 				foreach (string arq in dialogo.FileNames)
 				{
-					arquivoAnexadoPeloAgente.Add(arq); // Adiciona o caminho do arquivo à lista
-					btnAnexar.Text = "Anexar*"; // Indica que há arquivos anexados
+					arquivoAnexadoPeloAgente.Add(arq); 
+					btnAnexar.Text = "Anexar*";
 				}
 			}
 		}
